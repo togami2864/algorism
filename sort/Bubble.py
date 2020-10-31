a2d57e5515a95f087d62ce7610751a84e4725e86
Yoshiaki Togami <tuabobo123@gmail.com>
2020-10-31 14:07:55 +0900

    Add bubble sort

---
+++ sort/Bubble.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def bubble_sort(numbers: List[int]) -> List[int]:
+    for i in range(len(numbers)):
+        for j in range((len(numbers)-1)-i):
+            if numbers[j] > numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+    return numbers
+
+
+if __name__ == '__main__':
+    import random
+    nums = [random.randint(0, 1000) for _ in range(10)]
+    print(bubble_sort(nums))
